a9e33ae83342c4589c2d4864111506c06a9054ec
bernoudes <lucasbernoudes@gmail.com>
2022-01-10 23:07:01 -0300

    Inicio dos Componetes de listas na tela de chat, alguns ajustes de css no ContentChat, Chat e MainPage

---
+++ APS_01_2021/Components/ContentChatViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APS_01_2021.Components
+{
+    public class ContentChatViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ APS_01_2021/Components/ListMeetingViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APS_01_2021.Components
+{
+    public class ListMeetingViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
